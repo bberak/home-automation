a953629f96088863dd12a9cb22d397de9945ca24
bberak <bberak@gmail.com>
2020-01-28 23:38:05 +1100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const Static = require('koa-static');
+const Koa = require('koa');
+const Router = require('@koa/router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/blah', (ctx, next) => {
+  ctx.body = 'Hello World';
+});
+
+app
+  .use(Static('public'))
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(80);
